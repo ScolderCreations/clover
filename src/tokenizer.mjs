f045d52a7a58b88e1fd3cb3d8355ee114f62a13f
Scolder <69083943+ScolderCreations@users.noreply.github.com>
2022-03-02 13:46:29 -0800

    more coed

--- src/tokenizer.mjs
+++ src/tokenizer.mjs
@@ -1,13 +1,18 @@
-import { token } from '../lib/tokens.mjs';
+import { token } from "../lib/tokens.mjs";
 
 function tokenize(str) {
-  var tr = Array()
-  var currentLetter='', currentText='';
+  var tr = Array();
+  var currentLetter = "",
+    currentText = "";
   for (let i = 0; i < str.length; i++) {
     currentLetter = str[i];
-    currentText+=currentLetter;
-    if (token.includes(currentText)) {
-      tr.push(token)
+    currentText += currentLetter;
+    if (/[ ;\s\n]/.match(currentLetter)) {
+      currentText = "";
+    }
+    if (token.keys.includes(currentText)) {
+      var tk = new Object(token[currentText][0], token[currentText][1]);
+      tr.push(tk);
     }
   }
 }

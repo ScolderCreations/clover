25037a2451b3d7f63ae0f8b7b37294c774646332
Scolder <69083943+ScolderCreations@users.noreply.github.com>
2022-03-03 10:37:14 -0800

    renaming

--- src/tokenizer.mjs
+++ src/tokenizer.mjs
@@ -1,18 +1,19 @@
-import { token } from "../lib/tokens.mjs";
+import { token } from "../resources/tokens.mjs"; // get tokens
 
 function tokenize(str) {
   var tr = Array();
   var currentLetter = "",
     currentText = "";
-  for (let i = 0; i < str.length; i++) {
+  for (let i = 0; i < str.length; i++) { // for index (char) of code
     currentLetter = str[i];
     currentText += currentLetter;
-    if (/[ ;\s\n]/.match(currentLetter)) {
+    if (/[ ;\s\n]/.match(currentLetter)) { // if is whitespace
       currentText = "";
     }
     if (token.keys.includes(currentText)) {
-      var tk = new Object(token[currentText][0], token[currentText][1]);
+      var tk = JSON.parse(`{${token[currentText][0]}: ${token[currentText][1]}}`); // {token: type}
       tr.push(tk);
     }
   }
+  return tr;
 }

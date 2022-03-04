cc12e5d412014a910aaf29b6acf81482e6bc0c05
Scolder <69083943+ScolderCreations@users.noreply.github.com>
2022-03-04 11:57:00 -0800

    param support?

--- src/commands.mjs
+++ src/commands.mjs
@@ -1,10 +1,11 @@
 import * as builtins from '../resources/builtins.mjs';
+import { token } from '../resources/tokens.mjs';
 /**
  * @param {string} func 
  * @param {Array} params 
  */
 function exec(func, params) {
   if (builtins[func] != undefined) {
-    return builtins[func]();
+    return builtins[func](params);
   }
 }
